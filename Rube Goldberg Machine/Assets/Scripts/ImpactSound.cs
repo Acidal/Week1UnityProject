9e5eedbdc38d3fc629b45f333c1acd0ae416827c
Connor Wilmot <connor.wilmot@surreyschools.ca>
2022-10-04 10:14:04 -0700

    Github Ignore Change

---
+++ Rube Goldberg Machine/Assets/Scripts/ImpactSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour
+{
+    public AudioSource impactSound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 1)
+        {
+            impactSound.Play();
+        }
+    }
+
+}
